557df85ca515cd5756a9ceab0e7498a58018b6f8
Scy <Scyless@protonmail.com>
2021-03-16 19:33:33 +0100

    Changed language to German, rewrote !quote list, added proper token
    Language now German, rewrote !quote list so that it now works correctly, added proper TOKEN, integrated .git

--- ducksquad.py
+++ ducksquad.py
@@ -1,16 +1,24 @@
 '''
 Created on Jun 27, 2019
-Updated to new Python 3.7 and discord.py rewrite on Jul 4, 2019
+Updated to new Python 3.9 and discord.py rewrite on Jul 4, 2019
 Python version 3.7
 Discord.py version 1.2.3
 @name: DuckSquad Bot
-@authors: Scy & Mongokyou
+@authors: Scy & Some Ideas by Mongokyou
 '''
 
 import discord
+from discord import File
 from discord.ext import commands
+from discord.voice_client import VoiceClient
+from discord.utils import get
+import youtube_dl
 import random
 import re
+import os
+from bs4 import BeautifulSoup
+import requests
+import textwrap
 
 duck = commands.Bot(command_prefix='!')
 duck.remove_command('help')
@@ -34,21 +42,28 @@ async def help(ctx):
 @duck.command()
 async def quack(ctx):
 
-    embed = discord.Embed(title="Seems like you quackes for help. Maybe there's still some hope left for you.", description="Note: This bot is not meant to be taken seriously. If you're offended by any jokes, that's on you.", color=0xff9000)
-    embed.add_field(name="!about", value="Information about the bot and why it exists.", inline=False)
-    embed.add_field(name="!ducks", value="Sends the best animal.", inline=False)
-    embed.add_field(name="!shitpost", value="Sends bad jokes written by 'Mongokyou' or 'Lil Tupac' on Discord.", inline=False)
-    embed.add_field(name="!rps", value="A rock paper scissors game. example: *!rps rock*, *!rps paper*", inline=False)
-    embed.add_field(name="!gopnik", value="Sends various gopniks that represent you. No offense towards russians, I love their culture.", inline=False)
-    embed.add_field(name="!hardbass", value="Sends hardbass via youtube links.", inline=False)
-    embed.add_field(name="!spirit", value="Shows your real spirit animal. Idea and pictures by 'Mongokyou' or 'Lil Tupac' on Discord.", inline=False)
+    embed = discord.Embed(title="Scheint so als hättest du Probleme dir alle Befehle zu merken, quack.", description="Note: This bot is not meant to be taken seriously. If you're offended by any jokes, that's on you.", color=0xff9000)
+    embed.add_field(name="!about", value="Infos zum Bot.", inline=False)
+    embed.add_field(name="!ducks", value="Sendet das beste Tier.", inline=False)
+    embed.add_field(name="!shitpost", value="Sendet schlechte Witze die von 'Mongokyou' geschrieben wurden.", inline=False)
+    embed.add_field(name="!rps", value="Stein Schere Papier. Beispiel: !rps `rock` oder !rps `paper`  Duh.", inline=False)
+    embed.add_field(name="!gopnik", value="Sendet verschiedene gopniks die dich representieren.", inline=False)
+    embed.add_field(name="!hardbass", value="Sendet hardbass via youtube links.", inline=False)
+    embed.add_field(name="!spirit", value="Sendet dir dein persönliches Spirit Animal. Idee und Bilder von 'Mongokyou'.", inline=False)
     embed.add_field(name="!ping", value="Pong.", inline=False)
-    embed.add_field(name="!coinflip", value="Flips a coin. Idea by 'Mongokyou' or 'Lil Tupac' on Discord.", inline=False)
-    embed.add_field(name="!anime", value="Recommends the one and only anime. Idea by 'Mongokyou' or 'Lil Tupac' on Discord.", inline=False)
-    embed.add_field(name="!hug", value="Hugs the person you mention. Idea by 'Mongokyou' or 'Lil Tupac' on Discord. Example: *!hug @Scyless", inline=False)
-    embed.add_field(name="!cat", value="Sends cat. Idea and pictures by 'Mongokyou' or 'Lil Tupac' on Discord.", inline=False)
-    embed.add_field(name="!hurensohn", value="Bot that won in the Discord Hack Week 2019. Fuck that.", inline=False)
-
+    embed.add_field(name="!coinflip", value="Wirft eine Münze. Idee von 'Mongokyou'.", inline=False)
+    embed.add_field(name="!anime", value="Empfiehlt dir den einzig wahren Anime. Idee von 'Mongokyou'.", inline=False)
+    embed.add_field(name="!hug", value="Umarmt die Person die erwähnt wird. Beispiel: !hug `@Scy` Idee von 'Mongokyou'.", inline=False)
+    embed.add_field(name="!hurensohn", value="Bot der die Discord Hack Week 2019 gewonnen hat. DAS ist wahrhaftig ein Hurensohn.", inline=False)
+    embed.add_field(name="!song", value="Offizieller Enten Squad Song.", inline=False)
+    embed.add_field(name="!decide", value="""Schreib !decide `a` or `b` und ich werde für dich die richtige Entscheidung treffen.""", inline=False)
+    embed.add_field(name="!rate", value="Bewertet deine Waifu. Beispiel: !rate `asuna`", inline=False)
+    embed.add_field(name="!quote", value="Schreib !quote `add *Text*` um ein Zitat hinzuzufügen, !quote `show` für ein zufälliges Zitat und !quote `list` um alle Zitate zu sehen.", inline=False)
+    embed.add_field(name="!vote", value="Für die 3 Uhr Morgens Ja/Nein Umfragen. Beispiel: !vote `Bin ich ein guter Bot?`", inline=False)
+    embed.add_field(name="!f oder !F", value="F", inline=False)    
+    embed.add_field(name="!cat oder !cats", value="Sendet 'ne Katze aus einer Liste. Idee und Bilder von 'Mongokyou'.", inline=False)
+    embed.add_field(name="!shiba", value="Sendet Shibas. Und meine Fresse sind die *süß.*", inline=False)
+    embed.set_footer(text='!barth')
     await ctx.send(embed=embed)
     
     
@@ -57,19 +72,21 @@ async def quack(ctx):
 async def about(ctx):
 
 
-    embed=discord.Embed(title="Please follow me on Twitter, I need followers", description="", color=0xff9000)
-    embed.set_author(name="Made by Scy", url="https://www.twitter.com/_Scyless", icon_url="https://i.imgur.com/lk60HXI.gif")
+    embed=discord.Embed(title="Folg mir auf Twitter, bin Influencer", description="", color=0xff9000)
+    embed.set_author(name="Made by Scy", url="https://www.twitter.com/ScyIess", icon_url="https://i.imgur.com/lk60HXI.gif")
     embed.set_thumbnail(url="https://i.imgur.com/0iUtk4b.png")
-    embed.add_field(name='Why was this Bot created?', value='This bot was made during the 2019 Discord Hack Week.', inline=True)
-    embed.add_field(name='Did it win?', value='No. A K-Pop Bot won. Fuck K-Pop.', inline=True)
-    embed.set_footer(text="Shoutout to Mongokyou for helping me create this bot :^⁾")
+    embed.add_field(name='Warum existierst du?', value='Der Bot wurde ursprünglich in 2017 aus eigener Interesse an Python erstellt, dann kam die Discord Hack Week 2019 und er wurde in rewrite neu geschrieben.', inline=True)
+    embed.add_field(name='Hat er gewonnen?', value='Ne. Aber ein K-Pop Bot der von \'nem 5 Jährigen kommen könnte. Fuck K-Pop.', inline=True)
+    embed.set_footer(text="Shoutout an Mongokyou für seine 2 Ideen (:")
     
     await ctx.send(embed=embed)
         
 
 # Sends ducks
-@duck.command()
+@duck.command(name='ducks',
+aliases=['duck'])
 async def ducks(ctx):
+
     
     ducks = [
         'https://i.imgur.com/ROdcHRQ.jpg',
@@ -331,8 +348,186 @@ async def hug(ctx):
     await ctx.send(random.choice(hugs) + ' ' + random.choice(links)) # Send random message with random gif
 
 
-# Sends nekos
+# Sends shitty bot link
 @duck.command()
+async def hurensohn(ctx):
+
+    await ctx.send('K-Pop was a mistake. ' + 'https://github.com/jmardjuki/KpopStan-bot')
+    
+
+@duck.command()
+async def play(ctx):
+    
+    author = ctx.message.author
+    channel = author.voice.channel
+     
+    site = 'https://www.youtube.com/results?search_query='
+    message = ctx.message.content
+    search = requests.get(site+message)
+    results = search.text
+    
+    soup=BeautifulSoup(results, 'html.parser')
+    videos = soup.findAll(attrs={'class':'yt-uix-tile-link'}) 
+    url=[]
+    
+    
+    for v in url:
+        tmp = 'https://www.youtube.com' + v['href']
+        videos.append(tmp)
+        result = re.sub("[\[\]\']", "", str(url[0:1]))
+        print(url)
+    await channel.connect()
+    discord.VoiceClient.play('/media/veracrypt1/Music/Annisokay [Incomplete]/1. Loud.mp3', after=None)
+    
+    
+@duck.command()
+async def stop(ctx):
+    for x in duck.voice_clients:
+            return await x.disconnect()
+
+    return await ctx.send('How am I supposed to stop if I haven\'t even begun?')
+        
+# Below here are only commands that have been rewritten from the old, LesserScy Bot
+
+
+@duck.command()
+async def song(ctx):
+        
+    song = ('''`
+    [Intro]
+He-he-here we go!
+
+So they\'re finally here, performing for you
+If you know the words, you can join in too
+Put your hands together, if you want to clap
+As we take you through this stupid rap!
+Huh!
+
+[Chorus]
+LS
+Loser Squad!
+
+[Verse 1]
+He\'s the leader of the bunch, you know him well
+He\'s finally back to kick some tail
+His Lesser Scy can‘t work in spurts!
+If he mentions ya, it's gonna hurt!
+He\'s bigger, slower, and weaker too
+He\'s the first member of the LS crew!
+Huh!
+
+[Chorus]
+LS
+Loser Squad!
+LS
+Loser Squad is here!
+
+[Verse 2]
+This Grill‘s got style, so listen up dudes
+She can sleep for hours, to suit her mood
+She's quick and nimble when she needs to be
+She can play through the night and get her blades!
+If you choose her, you'll choose wrong
+With a ron and a bo, she's one cool Girl!
+Huh!
+
+[Chorus]
+LS
+Loser Squad!
+
+[Verse 3]
+He has style, he no grace
+Th-this Boi has a owo face
+He can spam cute bois when he needs to
+And stretch his arms out, just to hug you
+Lachsfisch himself just like a balloon
+This crazy Boi just digs his tunes!
+Huh!
+    `''')
+    
+    await ctx.send(song + '\n \n`Credit: Adversary aka bu`:b::b:`er`')
+        
+        
+# Add something here
+@duck.command()
+async def decide(ctx):
+    umessage = ctx.message.content
+    fmessage = umessage.replace('!decide ', '')
+    udecision = re.split(' or ', fmessage)
+    fdecision = random.choice(udecision)
+    snark = [
+    '***{}*** sounds like the best choice.'.format(fdecision),
+    '***{}*** is the only sane option.'.format(fdecision),
+    '***{}*** is what a retard would pick. And you\'re retarded.'.format(fdecision),
+    
+    'Are you a retard? Of course you are. Pick ***{}***.'.format(fdecision),
+    'Pick your poison. Nevermind, I already did it for you. Choose ***{}***.'.format(fdecision),
+    'The Loser Squad demands you to take ***{}***.'.format(fdecision)
+    ]
+    await ctx.send(random.choice(snark))
+    
+
+# Add something here
+@duck.command()
+async def rate(ctx):
+    umessage = ctx.message.content
+    fmessage = umessage.replace('!rate ', '')
+    
+    await ctx.send('I\'d rate **{}** a **{}/10**.'.format(fmessage, random.randint(0, 10)))
+
+        
+# Add something here
+@duck.command()
+async def quote(ctx):
+    umessage = ctx.message.content
+    fmessage = umessage.replace('!quote ', '')
+    message = fmessage.replace('add ', '')[0:500]
+    database = open('./database/quotes', 'r').read()
+    
+    if 'add' in fmessage:
+        with open('./database/quotes', 'a') as file:
+            file.write(message + '\n')
+        await ctx.send('**' + message + '**' + ' wurde zur Datenbank hinzugefügt! Wehe es ist Spam, ich muss den Scheiß manuell entfernen.')
+    elif 'show' in fmessage:
+        await ctx.send('Zitat des Tages: \n \n`{}`'.format(random.choice(database.splitlines())))
+    elif 'list' in fmessage:
+        with open("./database/quotes", "rb") as quotes:
+            await ctx.send('Schau DM.')
+            await ctx.author.send('Hier sind \'se Chef.', file=discord.File(quotes, "./database/quotes"))
+        
+@duck.command()
+async def dm(ctx):
+    await ctx.send('Schau DM.')
+    with open("./database/quotes", "rb") as file:
+        await ctx.author.send('Hier sind \'se Chef.', file=discord.File(file, "./database/quotes"))
+
+# Add something here
+@duck.command()
+async def vote(ctx):
+    umessage = ctx.message.content
+    fmessage = umessage.replace('!vote ', '')
+    
+    thonk = discord.PartialEmoji(id = '357118655504580609', name = 'thonk', animated=False)
+    upvote = discord.PartialEmoji(id = '266219215390900225', name = 'upvote', animated=False)
+    downvote = discord.PartialEmoji(id = '266219202384363530', name = 'downvote', animated=False)
+    
+    msg = await ctx.send(f'{fmessage} {thonk}')
+    await msg.add_reaction(upvote)
+    await msg.add_reaction(downvote)
+
+
+# Add something here
+@duck.command(name='f',
+aliases=['F'])
+async def f(ctx):
+    
+    await ctx.message.delete()
+    await ctx.send('{} paid his respects.'.format(ctx.message.author.mention))
+    await ctx.add_reaction()
+
+# Sends nekos
+@duck.command(name='cat',
+aliases=['cats'])
 async def cat(ctx):
     
     cats = [
@@ -354,23 +549,25 @@ async def cat(ctx):
         'https://i.imgur.com/sanA2uk.jpg',
         'https://i.imgur.com/FvxToA9.jpg'
         ]
+
     
     await ctx.send(random.choice(cats))
-
-
-# Sends shitty bot link
+    
+    
 @duck.command()
-async def hurensohn(ctx):
-
-    await ctx.send('K-Pop was a mistake. ' + 'https://github.com/jmardjuki/KpopStan-bot')
+async def shiba(ctx):    
+    shiba = requests.get('http://shibe.online/api/shibes?count=[1]&urls=[true]&httpsUrls=[true]')
+    msg = await ctx.send(shiba.json())
+    msg2 = re.sub('[\[\]\']', '', msg.content)
+    
+    await msg.edit(content = 'https://cdn.shibe.online/shibes/{}.jpg'.format(msg2))
     
     
 # Secret command
 @duck.command()
 async def barth(ctx):
+    await ctx.send('https://cps-static.rovicorp.com/3/JPG_400/MI0004/190/MI0004190678.jpg?partner=allrovi.com')
 
-    await ctx.send('KENNSTE KENNSTE?? https://cps-static.rovicorp.com/3/JPG_400/MI0004/190/MI0004190678.jpg?partner=allrovi.com')
 
-
-TOKEN = 'NTkzNzA2NzI2NjM3Njk5MDgz.XRdFrw.zYlB9wQEvNriv6RdOhFfGK1PhJ8'
-duck.run(TOKEN)
+token = open("./.TOKEN","r").readline()
+duck.run(token)
